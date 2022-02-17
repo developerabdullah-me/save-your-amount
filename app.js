e3ade2ab07ec497139f57dd652160b7ebd6c3065
developerabdullah-me <abdullahtalha9999@gmail.com>
2022-02-17 12:06:24 +0600

    js full

---
+++ app.js
@@ -0,0 +1,36 @@
+document.getElementById('calclaute').addEventListener('click', function(){
+     const incomes=document.getElementById('income').value;
+    //  incomes.value;
+     const inputFood=document.getElementById('food').value;
+     const inputRent=document.getElementById('rent').value;
+     const inputclothes=document.getElementById('clothes').value;
+     const totalCalclaute=parseFloat(inputFood)+parseFloat(inputRent)+parseFloat(inputclothes);
+    // console.log(totalCalclaute);
+   const totaltalExpenses=document.getElementById('totaltal-expenses');
+    totaltalExpenses.innerText = totalCalclaute;
+    //  const b=incomes-totaltalExpenses;
+     let p=document.getElementById('balance');
+     p.innerText=incomes-totaltalExpenses.innerText;
+     
+})
+
+document.getElementById('save-btn').addEventListener('click',function(){
+     const incomes=document.getElementById('income').value;
+     const k=document.getElementById('save').value;
+
+     let e= parseFloat(incomes / 100)*parseFloat(k);
+
+    
+     const s = document.getElementById('saving-amount');
+        s.innerText=e;
+
+        let p=document.getElementById('balance');
+         p.innerText;
+       const saveMinus= parseFloat(p.innerText)-parseFloat(s.innerText);
+
+        const d=document.getElementById('remening-balance')
+            d.innerText=saveMinus;
+    //  const saveparsent=document.getElementById('save-parsent').innerText;
+    
+})
+
